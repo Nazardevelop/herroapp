a8e842694339d7dbe038d161d935dbfa5cfde5ff
Nazar Tolmachov <tolmachovnazar@gmail.com>
2017-08-09 19:11:41 +0300

    first step hero app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './models/Hero';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  hero:Hero = {
+    id: 1,
+    name:'Windstorm'
+  };
+  title = 'Tour of Heroes';
 }
